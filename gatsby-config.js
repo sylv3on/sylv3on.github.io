85fad01e90aa1c4e547b2c288195e424bd488d4b
sylv3on <ashley24241@hotmail.com>
2020-05-14 14:35:58 -0400

    Update gatsby-config.js
    adding google analytics trackingID

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: 'UA-XXXXXXXXX-X'
+        trackingId: 'UA-160775640-1'
       }
     }
   ]
